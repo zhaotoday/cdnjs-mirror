ab5eaaae1cd240d45cc00b8388ce4076abac36dd
zhaotoday <729234283@qq.com>
2016-12-10 21:10:35 +0800

    format

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 $url = 'http://cdnjs.cloudflare.com/ajax/libs/' . $_GET['lib'];
 $headers = get_headers($url, 1);
 
-header('Content-Type: '.$headers['Content-Type']);
+header('Content-Type: ' . $headers['Content-Type']);
 echo file_get_contents($url);
