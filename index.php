ac06eac4fd5d36e4c8a40e2572419732876f5b40
zhaotoday <729234283@qq.com>
2016-12-10 21:00:20 +0800

    get file content using php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$url = 'http://cdnjs.cloudflare.com/ajax/libs/' . $_GET['lib'];
+$headers = get_headers($url, 1);
+
+header('Content-Type: '.$headers['Content-Type']);
+echo file_get_contents($url);
